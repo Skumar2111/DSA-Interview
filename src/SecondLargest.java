cc9cb8644d3c996dec1ffee288a9f974a27b01ba
Sushant Kumar <sushant.k052@gmail.com>
2026-05-14 22:40:11 +0530

    Initial commit

---
+++ src/SecondLargest.java
@@ -0,0 +1,20 @@
+public class SecondLargest {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,5};
+
+        int max = Integer.MIN_VALUE;
+        int secondMax = 0;
+        for(int i = 0 ; i < arr.length ; i++)
+        {
+            if(arr[i] > max)
+            {
+                secondMax = max;
+                max = arr[i];
+            }
+        }
+        System.out.println(secondMax);
+
+    }
+}
