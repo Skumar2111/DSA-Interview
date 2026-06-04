ae6319e32b8637b2f6e571173e4e4fcd8de01c1c
Sushant Kumar <sushant.k052@gmail.com>
2026-06-04 22:33:34 +0530

    Initial commit

---
+++ src/LogicBuilding/EvenOdd.java
@@ -0,0 +1,18 @@
+package LogicBuilding;
+
+public class EvenOdd {
+
+    public static void main(String[] args) {
+
+        int number = 21;
+
+        if(number % 2 == 0)
+        {
+            System.out.println("true");
+        }
+        else
+        {
+            System.out.println("false");
+        }
+    }
+}
