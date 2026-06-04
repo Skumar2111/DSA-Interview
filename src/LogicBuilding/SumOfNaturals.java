06e3d2b54890dc026f2126bc503066338d2f8522
Sushant Kumar <sushant.k052@gmail.com>
2026-06-04 22:43:11 +0530

    Initial commit

---
+++ src/LogicBuilding/SumOfNaturals.java
@@ -0,0 +1,17 @@
+package LogicBuilding;
+
+public class SumOfNaturals {
+
+    public static void main(String[] args) {
+
+        int number = 100;
+
+        int sum = (number * number+1) / 2;
+
+        System.out.println(sum);
+
+    }
+
+
+
+}
