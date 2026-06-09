7532d4508feab7121586693081a9449e63263106
Sushant Kumar <sushant.k052@gmail.com>
2026-06-09 23:52:25 +0530

    Initial commit

---
+++ src/LogicBuilding/GCDOptimal.java
@@ -0,0 +1,27 @@
+package LogicBuilding;
+
+public class GCDOptimal {
+
+    public static void main(String[] args) {
+
+        int a = 5 ;
+        int b = 20;
+
+        System.out.println(gcd(a,b));
+
+
+    }
+
+    private static int gcd(int a, int b) {
+
+        if(b == 0)
+        {
+            return a;
+        }
+        else
+        {
+            return gcd(b,a%b);
+        }
+
+    }
+}
