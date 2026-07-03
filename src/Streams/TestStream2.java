fbdc044787c2985e1f483826679cc161d25f10e0
Sushant Kumar <sushant.k052@gmail.com>
2026-07-03 10:09:32 +0530

    Initial commit

--- src/Streams/TestStream2.java
+++ src/Streams/TestStream2.java
@@ -6,6 +6,7 @@ import java.util.Random;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class TestStream2 {
 
@@ -52,5 +53,34 @@ public class TestStream2 {
 
         System.out.println(list.stream().map((number)-> number+(-5)).collect(Collectors.toList()));
 
+
+        List<Integer> newOne = new ArrayList<>();
+        newOne.add(10);
+        newOne.add(20);
+        newOne.add(30);
+        newOne.add(21);
+
+        System.out.println(newOne.stream().distinct().collect(Collectors.toList()));
+
+        System.out.println(newOne.stream().limit(2).collect(Collectors.toList()));
+        System.out.println(newOne.stream().limit(2).collect(Collectors.toList()));
+
+
+        IntStream.iterate((int) 'A', i -> i <= (int) 'z', i -> i+1).
+         filter(Character::isAlphabetic).filter(Character::isUpperCase)
+                .filter(i -> Character.toUpperCase(i) < 'E')
+                .skip(2)
+        .forEach(d -> System.out.printf("%c ",d));
+
+
+      //  Stream.generate(() -> random.ints(20,5,20)).sorted().forEach(System.out::println);
+
+
+        var result = IntStream.iterate(0,i -> i  <=1000 , i -> i=i+21).summaryStatistics();
+        System.out.println(result);
+
+
+        var leapYear = IntStream.iterate(2000, i -> i <=2026 , i -> i += 1).filter(i -> i % 4 == 0).peek(System.out::println).summaryStatistics();
+        System.out.println("Leap year details : "+leapYear);
     }
 }
