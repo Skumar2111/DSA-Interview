7da27d448c451945ae1d3f7a13ed6d3a3b974e5d
Sushant Kumar <sushant.k052@gmail.com>
2026-07-03 22:55:20 +0530

    Initial commit

---
+++ src/Recursion/RopeCut.java
@@ -0,0 +1,35 @@
+package Recursion;
+
+public class RopeCut {
+
+    public static void main(String[] args) {
+        int rope_length = 9;
+        int a = 2;
+        int b = 2;
+        int c = 2;
+
+        System.out.println(calculateSum(rope_length,a,b,c));
+    }
+
+    private static int calculateSum(int rope_length, int a, int b, int c) {
+
+        if(rope_length == 0)
+        {
+            return 0;
+        }
+
+        if(rope_length < 0)
+        {
+            return -1;
+        }
+
+        int max_a_b = Math.max(calculateSum(rope_length-a,a,b,c),calculateSum(rope_length-b,a,b,c));
+        int max_c = Math.max(max_a_b,calculateSum(rope_length-c,a,b,c));
+
+        if(max_c == -1)
+        {
+            return -1;
+        }
+        return max_c+1;
+    }
+}
