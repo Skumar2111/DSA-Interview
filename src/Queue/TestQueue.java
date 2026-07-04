7716cd256113ec5b0455d85da52c1bd7ce04a133
Sushant Kumar <sushant.k052@gmail.com>
2026-07-04 21:36:20 +0530

    Initial commit

---
+++ src/Queue/TestQueue.java
@@ -0,0 +1,27 @@
+package Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TestQueue {
+
+    public static void main(String[] args) {
+
+        Queue<Integer> queue = new LinkedList<>();
+
+
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        queue.offer(40);
+
+        while(!queue.isEmpty())
+        {
+            System.out.println("Polling the element from queue ... "+queue.poll());
+            if(queue.peek()!=null) {
+                System.out.println("Next element to be polled ... " + queue.peek());
+            }
+        }
+
+    }
+}
