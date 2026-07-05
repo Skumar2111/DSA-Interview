e10620ba36cd3bdc50775d6a3d1b0ec04029f5df
Sushant Kumar <sushant.k052@gmail.com>
2026-07-05 21:05:30 +0530

    Initial commit

---
+++ src/Stack/StockSpan.java
@@ -0,0 +1,37 @@
+package Stack;
+
+import java.util.Stack;
+
+public class StockSpan {
+
+    public static void main(String[] args) {
+
+        int[] stocks = {60,10,20,15,35,50};
+
+        stockSpan(stocks);
+
+    }
+
+    private static void stockSpan(int[] stocks) {
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        System.out.println(1);
+
+        int span;
+        for(int i = 1 ; i < stocks.length ; i++)
+        {
+            while(stack.isEmpty() && stocks[stack.peek()] <= stocks[i])
+            {
+                stack.pop();
+            }
+
+            span = stack.isEmpty()?i+1:i - stack.peek();
+
+            System.out.println(span);
+
+            stack.push(i);
+
+        }
+    }
+}
