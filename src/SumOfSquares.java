1c1ded25ed87b8b75e5d7c97b5f1477f8e897251
Sushant Kumar <sushant.k052@gmail.com>
2026-07-06 22:16:08 +0530

    Initial commit

---
+++ src/SumOfSquares.java
@@ -0,0 +1,10 @@
+public class SumOfSquares {
+
+    public static void main(String[] args) {
+        int n = 10;
+
+        int result = ((n) * (n+1) * (2*n + 1))/6;
+
+        System.out.println(result);
+    }
+}
