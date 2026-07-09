7d6dd7de60eb1078a1d1b98207a2201dd3a261bc
Sushant Kumar <sushant.k052@gmail.com>
2026-07-09 10:14:40 +0530

    Initial commit

--- src/ThreadsManage/ExecutorsNew.java
+++ src/ThreadsManage/ExecutorsNew.java
@@ -12,6 +12,8 @@ public class ExecutorsNew {
 
         var executor = Executors.newWorkStealingPool(5);
 
+        var executorCallable = Executors.newFixedThreadPool(3);
+
         Runnable runnable = () ->
         {
             for (int i = 0; i < 10000; i++) {
@@ -35,6 +37,26 @@ public class ExecutorsNew {
         List<Integer> list = IntStream.rangeClosed(0,50).boxed().toList();
 
         list.parallelStream().forEach(i -> System.out.println(i + " printing for " +Thread.currentThread()));
+
+
+        Callable<Integer> taskCallable = () ->
+        {
+            System.out.println("Executing on Thread : " +Thread.currentThread().getName());
+            return 1000;
+        };
+
+        Future<Integer> futureTask = executorCallable.submit(taskCallable);
+
+        try {
+            Integer result = futureTask.get();
+            System.out.println("This is from future callable :"+result);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        executorCallable.shutdown();
 
     }
 }
