ea9a2d1ea8646be8d8cec9c19aa8797951cf910e
Sushant Kumar <sushant.k052@gmail.com>
2026-07-11 00:43:57 +0530

    Initial commit

---
+++ src/Anagram.java
@@ -0,0 +1,39 @@
+public class Anagram {
+
+    public static void main(String[] args) {
+        String s1 = "listen";
+        String s2 = "silent";
+
+
+        System.out.println(isAnagram(s1,s2));
+
+
+    }
+
+    private static boolean isAnagram(String s1, String s2) {
+
+        if(s1.length() != s2.length())
+        {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for(int i = 0 ; i < s1.length() ; i++)
+        {
+            count[s1.charAt(i) - 'a']++;
+            count[s2.charAt(i) - 'a']--;
+        }
+
+
+        for(int c : count)
+        {
+            if(c != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
