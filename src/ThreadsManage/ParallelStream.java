40e4322b051ec8f6efedf3fa3001e1637467b996
Sushant Kumar <sushant.k052@gmail.com>
2026-07-11 07:30:39 +0530

    Initial commit

--- src/ThreadsManage/ParallelStream.java
+++ src/ThreadsManage/ParallelStream.java
@@ -1,7 +1,8 @@
 package ThreadsManage;
 
-import java.util.Comparator;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 record Person(String firstName, String lastName, int age)
@@ -35,6 +36,26 @@ public class ParallelStream {
     public static void main(String[] args) {
 
 
-        Stream.generate(Person::new).limit(100).parallel().sorted(Comparator.comparing(Person::lastName)).forEach(System.out::println);
+        Stream.generate(Person::new).limit(100).parallel().sorted(Comparator.comparing(Person::lastName)).forEachOrdered(System.out::println);
+
+
+
+        int sumWithoutParallel = IntStream.range(0,1000).sum();
+        System.out.println("Sum without parallel "+sumWithoutParallel);
+
+        int sumWithParallel = IntStream.range(0,1000).parallel().reduce(0,Integer::sum);
+        System.out.println("Sum with parallel "+sumWithParallel);
+
+        Map<String, Long> lastNameCount = Collections.synchronizedMap(new HashMap<>());
+
+        lastNameCount = Stream.generate(Person::new).limit(10000).parallel().collect(Collectors.groupingByConcurrent(Person::firstName,Collectors.counting()));
+
+
+        lastNameCount.entrySet().stream().forEach(System.out::println);
+
+        System.out.println(lastNameCount.getClass().getName());
+
+
+
     }
 }
