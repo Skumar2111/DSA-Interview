d0c02d153095eaa8480288c66581ea4b13832fd3
Sushant Kumar <sushant.k052@gmail.com>
2026-07-13 11:14:52 +0530

    Exception

--- src/ExceptionHandling/TestingFile.java
+++ src/ExceptionHandling/TestingFile.java
@@ -1,27 +1,61 @@
 package ExceptionHandling;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.Date;
 
 public class TestingFile {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
 
         try {
-            BufferedReader br = new BufferedReader(new FileReader("./testing.txt"));
+            BufferedReader br  = new BufferedReader(new FileReader("testing.txt"));
 
+            String line;
 
-            while(br.readLine() != null)
+            while((line = br.readLine()) != null)
             {
-                System.out.println(br.readLine());
+                System.out.println(line);
             }
+
+            br.close();
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        
+        
+        codeForWriting("file");
+
+    }
+
+    private static void codeForWriting(String file) throws IOException {
+
+
+
+        try
+        {
+            Date date = new Date();
+            BufferedWriter bw = new BufferedWriter(new FileWriter("testing.txt"));
+            bw.write("Hey");
+            bw.newLine();
+            bw.write("Sushant");
+            bw.newLine();
+            bw.write("Sushant");
+            bw.newLine();
+            bw.write("==================================================");
+            bw.newLine();
+            bw.write(Long.toString(date.getTime()));
+
+
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
