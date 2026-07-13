cf3e3541e69925625e13462055a5c291f924d9db
Sushant Kumar <sushant.k052@gmail.com>
2026-07-13 10:27:40 +0530

    Exception

---
+++ src/ExceptionHandling/HandlingExcpetions.java
@@ -0,0 +1,22 @@
+package ExceptionHandling;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class HandlingExcpetions {
+
+    public static void main(String[] args)
+    {
+        String fileName = "testing.csv";
+        Path path = Paths.get(fileName);
+
+        try {
+            List<String> list = Files.readAllLines(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
