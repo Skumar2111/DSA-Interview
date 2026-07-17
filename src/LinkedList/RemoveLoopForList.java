0b38dc221584f883825f0194594be0d44eeb0dc6
Sushant Kumar <sushant.k052@gmail.com>
2026-07-17 18:23:29 +0530

    LL

---
+++ src/LinkedList/RemoveLoopForList.java
@@ -0,0 +1,58 @@
+package LinkedList;
+
+
+class Node {
+    int data;
+    Node next;
+    Node(int val) {
+        data = val;
+        next = null;
+    }
+}
+class RemoveLoopForList {
+
+    public static void main(String[] args) {
+
+        Node node = new Node(10);
+        Node node_1 = new Node(20);
+        Node node_2 = new Node(30);
+        Node node_3 = new Node(40);
+
+
+
+    }
+
+    public static void removeLoop(Node head) {
+        // code here
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast)
+            {
+                slow = head;
+                while(slow != fast)
+                {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+            }
+
+            Node temp = slow;
+            while(temp.next != slow)
+            {
+                temp = temp.next;
+            }
+
+            temp.next = null;
+        }
+
+
+
+
+    }
+}
