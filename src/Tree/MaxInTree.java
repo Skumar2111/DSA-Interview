ae24ae6f6b6a69322fa5c2fb2c1844de3a13836b
Sushant Kumar <sushant.k052@gmail.com>
2026-07-21 00:03:37 +0530

    Tree

---
+++ src/Tree/MaxInTree.java
@@ -0,0 +1,46 @@
+package Tree;
+
+public class MaxInTree {
+
+    Node root;
+
+    public static void main(String[] args) {
+
+        MaxInTree maxInTree = new MaxInTree();
+        maxInTree.root = new Node(10);
+        maxInTree.root.left = new Node(20);
+        maxInTree.root.right = new Node(30);
+        int initial = -1;
+        System.out.println(findMax(maxInTree.root));
+
+    }
+
+    private static int findMax(Node root) {
+
+        if(root == null)
+        {
+            return Integer.MIN_VALUE;
+        }
+
+        int result = root.data;
+
+        int l_result = findMax(root.left);
+        int r_result = findMax(root.right);
+
+        if(l_result > result)
+        {
+            result = l_result;
+        }
+        if(r_result > result)
+        {
+            result = r_result;
+        }
+
+        return result;
+
+
+
+    }
+
+
+}
