7fb448ea5a268b39e24d9a49d08f42993cc26a83
Sushant Kumar <sushant.k052@gmail.com>
2026-07-27 22:36:10 +0530

    DP

---
+++ src/DP/CountWays.java
@@ -0,0 +1,36 @@
+package DP;
+
+public class CountWays {
+
+    static int c = 0;
+    public static void main(String[] args) {
+
+        int[] coins = {1,2,3};
+        int sum = 4;
+        int n = coins.length;
+
+        int ways = countWays(coins,n,sum);
+
+        System.out.println(ways);
+        System.out.println(c);
+
+    }
+
+    private static int countWays(int[] coins, int n, int sum) {
+        c = c+1;
+        if(sum == 0)
+        {
+            return 1;
+        }
+        if(sum < 0)
+        {
+            return 0;
+        }
+        if(n == 0)
+        {
+            return 0;
+        }
+
+        return  countWays(coins,n,sum - coins[n-1]) + countWays(coins,n-1,sum);
+    }
+}
