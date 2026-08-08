13f0da1bc00ee9f0537a277aede1278ccfb5b7d4
Sushant Kumar <sushant.k052@gmail.com>
2026-08-08 11:54:38 +0530

    LeetCode - 75

---
+++ src/DP/DutchNationalFlag.java
@@ -0,0 +1,53 @@
+package DP;
+
+/*
+* LeetCode - 75 Sort Colors
+* https://leetcode.com/problems/sort-colors/description/
+* */
+
+
+public class DutchNationalFlag {
+    public static void main(String[] args) {
+        int[] nums = {2,0,2,1,1,0};
+        sortColors(nums);
+
+    }
+        public static void sortColors(int[] nums) {
+
+            int start = 0;
+            int mid = 0;
+            int end = nums.length - 1;
+
+            while(mid <= end)
+            {
+                switch(nums[mid])
+                {
+                    case 0:
+                        swap(nums, start , mid);
+                        start++;
+                        mid++;
+                        break;
+
+                    case 1:
+                        mid++;
+                        break;
+
+                    case 2:
+                        swap(nums,mid,end);
+                        end--;
+                        break;
+                }
+            }
+
+
+        }
+
+
+        public static void swap(int[] nums , int pos1 , int pos2)
+        {
+            int temp = nums[pos1];
+            nums[pos1] = nums[pos2];
+            nums[pos2] = temp;
+        }
+    }
+
