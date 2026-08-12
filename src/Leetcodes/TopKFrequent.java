fc16fd7622c0db57cd2e2c1cf76b9f17b9413325
Sushant Kumar <sushant.k052@gmail.com>
2026-08-12 22:23:02 +0530

    LeetCode - 347

---
+++ src/Leetcodes/TopKFrequent.java
@@ -0,0 +1,72 @@
+package Leetcodes;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/*
+* LeetCode - 347
+* */
+public class TopKFrequent {
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,2,2,3};
+        int k = 2;
+
+        int[] res = new int[k];
+        res = topKFrequent(nums,k);
+
+        for(int i = 0 ; i < k ; i++)
+        {
+            System.out.println(res[i]);
+        }
+
+    }
+
+
+        public static int[] topKFrequent(int[] nums, int k) {
+
+            HashMap<Integer,Integer> hmap = new HashMap<>();
+
+
+            int[] result = new int[k];
+
+            for(int i = 0 ; i < nums.length ; i++)
+            {
+                if(hmap.containsKey(nums[i]))
+                {
+                    hmap.put(nums[i], hmap.get(nums[i])+1);
+                }
+                else
+                {
+                    hmap.put(nums[i],1);
+                }
+            }
+
+
+
+            PriorityQueue<Map.Entry<Integer, Integer>> pq =
+                    new PriorityQueue<>(
+                            (a, b) -> b.getValue() - a.getValue()
+                    );
+
+            int i = 0;
+            // 3. Keep only k most frequent elements
+            for (Map.Entry<Integer, Integer> entry : hmap.entrySet()) {
+
+                pq.offer(entry);
+
+                if (pq.size() > k) {
+
+                    result[i] = pq.poll().getKey();
+                    i++;
+                }
+
+
+
+            }
+            return result;
+        }
+    }
+
+
