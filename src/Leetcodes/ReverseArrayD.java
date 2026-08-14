a0a25ce86269fadb09b6a46bd6c78ee7e96c4bc3
Sushant Kumar <sushant.k052@gmail.com>
2026-08-14 21:57:07 +0530

    Left rotate by 189

--- src/Leetcodes/ReverseArrayD.java
+++ src/Leetcodes/ReverseArrayD.java
@@ -1,9 +1,38 @@
 package Leetcodes;
 
+/*
+* Left rotate by 189
+* */
 public class ReverseArrayD {
 
     public static void main(String[] args) {
 
+        int[] nums = {1,2,3,4,5};
+        int k = 3;
+        rotate(nums,k);
 
     }
-}
+
+        public static void rotate(int[] nums, int k) {
+
+            reverse(nums,0,k-1);
+            reverse(nums,k,nums.length-1);
+            reverse(nums,0,nums.length - 1);
+
+
+
+        }
+
+        public static void reverse(int[] arr , int start , int end)
+        {
+            if(start < end)
+            {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+
