e86800eb514887e0f6e54a9edec2231e86798540
Sushant Kumar <sushant.k052@gmail.com>
2026-09-22 06:11:16 +0530

    Sub sequence

--- src/DP/StringSubsequence.java
+++ src/DP/StringSubsequence.java
@@ -15,26 +15,43 @@ public class StringSubsequence {
         String input1 = "abdc";
         String input2 = "abca";
 
-        int lcs = calculateSubsequence(input1,input2,input1.length(),input2.length());
+        int[][] memo = new int[input1.length()+1][input2.length()+1];
+
+
+        for(int i = 0 ; i < input1.length()+1; i++)
+        {
+            for(int j = 0 ; j < input2.length()+1 ; j++)
+            {
+                memo[i][j] = -1;
+            }
+        }
+
+        int lcs = calculateSubsequence(input1,input2,input1.length(),input2.length(),memo);
 
         System.out.println(lcs);
 
     }
 
-    private static int calculateSubsequence(String input1, String input2, int m , int n) {
+    private static int calculateSubsequence(String input1, String input2, int m , int n , int[][] memo) {
 
         if(m == 0 || n == 0)
         {
             return 0;
         }
 
+        if(memo[m][n] != -1) return memo[m][n];
+
         if(input1.charAt(m-1) == input2.charAt(n-1))
         {
-            return 1 + calculateSubsequence(input1, input2, m - 1, n - 1);
+            memo[m][n] = 1 + calculateSubsequence(input1, input2, m - 1, n - 1, memo);
+
         }
         else
         {
-            return Math.max(calculateSubsequence(input1,input2,m,n-1),calculateSubsequence(input1,input2,m-1,n));
+            memo[m][n] =  Math.max(calculateSubsequence(input1,input2,m,n-1,memo),calculateSubsequence(input1,input2,m-1,n,memo));
+
         }
+
+        return memo[m][n];
     }
 }
